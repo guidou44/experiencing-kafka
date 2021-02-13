f89ae3da95bd68d250f935c84e2e74dd614273b9
guidou44 <guim_44@hotmail.com>
2021-02-13 09:30:27 -0500

    added bckground thread in startup app

--- backend/src/main/java/com/kafkatester/MainApplication.java
+++ backend/src/main/java/com/kafkatester/MainApplication.java
@@ -1,14 +1,18 @@
 package com.kafkatester;
 
+import com.kafkatester.domain.network.NetworkScanner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class MainApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MainApplication.class, args);
+    ApplicationContext context = SpringApplication.run(MainApplication.class, args);
+    Thread networkScannerWorker = new Thread(context.getBean(NetworkScanner.class));
+    networkScannerWorker.start();
   }
 }
